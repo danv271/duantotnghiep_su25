7fe7ddf49ec3caac789fa606690146e281e03a22
Nguyễn Đức Trường <truongndph50320@gmail.com>
2025-05-22 03:59:56 +0700

    Category and Product Detail

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.index');
 });
+Route::get('/checkout', function () {
+    return view('client.checkout');
+})->name('checkout');
+Route::post('/checkout', function () {
+    return view('client.checkout');
+})->name('checkout.process');
+Route::view('/cart', 'client.cart'); // Trang giỏ hàng hiển thị HTML
+Route::get('/product-detail', function () {
+    return view('client.product-detail');
+})->name('product-detail');
+Route::get('/category', function () {
+    return view('client.category');
+})->name('category');
