d01865b8c469bcf167538f89eb6e5ed1ee4c3fbf
Minhnv0610205 <nguyenvanminh0610205@gmail.com>
2025-05-21 11:47:29 +0700

    account

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/account', function () {
+    return view('account');
+})->name('account');
 Route::get('/checkout', function () {
     return view('checkout');
 })->name('checkout');
