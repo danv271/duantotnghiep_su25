119595ef01076a0bdd81226696312b862ba043bb
tuananhph51274 <anhndtph51274@gmail.com>
2025-07-02 22:01:17 +0700

    loi

--- app/Models/product.php
+++ app/Models/product.php
@@ -9,17 +9,5 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $table = 'products';
-    protected $primaryKey = 'id';
-    protected $fillable = ['name', 'description', 'category_id', 'base_price', 'created_at', 'updated_at'];
-
-    public function category(){
-        return $this->belongsTo(Category::class,"category_id","id");
-    }
-    public function images(){
-        return $this->hasMany(productsImage::class,"product_id");
-    }
-    public function variants(){
-        return $this->hasMany(variants::class,"product_id");
-    }
+    protected $fillable = ['name', 'description', 'category_id', 'base_price'];
 }
