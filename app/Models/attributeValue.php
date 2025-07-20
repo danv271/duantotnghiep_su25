06ab8cc1f589c7436041a88fa62037592bffe842
quocphong04 <quocphongg2004@gmail.com>
2025-07-20 18:40:47 +0700

    Đổi menu Attributes thành tiếng Việt: Thuộc tính, Danh sách, Thêm mới

--- app/Models/attributeValue.php
+++ app/Models/attributeValue.php
@@ -7,16 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class AttributeValue extends Model
 {
-    use HasFactory;
+   use HasFactory;
 
     protected $table = 'attributes_values';
 
     protected $fillable = ['attribute_id', 'value'];
 
-    // Quan hệ với Attribute
     public function attribute()
     {
-        return $this->belongsTo(Attribute::class, 'attribute_id');
+        return $this->belongsTo(Attribute::class);
     }
     public function variants(){
         return $this->belongsToMany(variants::class,'variant_attributes','attribute_value_id','variant_id');
