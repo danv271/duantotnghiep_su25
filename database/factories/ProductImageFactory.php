57174abbc8f82f523a632ec303d874f99f702413
tuananhph51274 <anhndtph51274@gmail.com>
2025-08-25 20:37:19 +0700

    test

--- database/factories/ProductImageFactory.php
+++ database/factories/ProductImageFactory.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Factories;
 
 use App\Models\Product;
@@ -12,10 +11,22 @@ class ProductImageFactory extends Factory
 
     public function definition(): array
     {
+        // Dùng đường dẫn thật để Faker có thể ghi file
+        $realPath = storage_path('app/public/images/products');
+
+        // Nếu thư mục chưa tồn tại thì tạo mới
+        if (!is_dir($realPath)) {
+            mkdir($realPath, 0755, true);
+        }
+
+        // Tạo ảnh và lấy tên file
+        $filename = $this->faker->image($realPath, 640, 480, null, false);
+
         return [
             'product_id' => Product::inRandomOrder()->value('id'),
-            'path' => 'images/products/' . $this->faker->image('public/storage/images/products', 640, 480, null, false),
-            'is_featured' => $this->faker->boolean(20), // 20% là ảnh nổi bật
+            'path' => 'images/products/' . basename($filename), // lưu đường dẫn sau /storage
+            'is_featured' => $this->faker->boolean(20),
         ];
     }
 }
+
